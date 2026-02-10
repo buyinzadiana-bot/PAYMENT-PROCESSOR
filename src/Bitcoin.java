54623e1e7ac1099f3f6aa7a28b7ad271b04fcfd7
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-10 14:25:08 +0200

    Initialised all classes I will be needing

---
+++ src/Bitcoin.java
@@ -0,0 +1,11 @@
+public class Bitcoin {
+    private String walletAddress;
+
+    public Bitcoin(String walletAddress) {
+        this.walletAddress = walletAddress;
+    }
+
+    public String getWalletAddress() {
+        return walletAddress;
+    }
+}
