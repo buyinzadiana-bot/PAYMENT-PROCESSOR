3b40bd21f4bdbab538613fcd0b699420826fd6d3
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-10 14:25:53 +0200

    Initialised all classes I will be needing

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        PaymentProcessor processor = new PaymentProcessor();
+
+        CreditCard card = new CreditCard("1234-5678-9012");
+        PayPal paypal = new PayPal("user@example.com");
+        Bitcoin bitcoin = new Bitcoin("bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh");
+
+        processor.processPayment(card);
+        System.out.println();
+
+        processor.processPayment(paypal);
+        System.out.println();
+
+        processor.processPayment(bitcoin);
     }
 }
